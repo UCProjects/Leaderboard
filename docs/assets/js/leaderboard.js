3745ffbf8906ecc3bf692fcc53e03ce91d10db2b
feildmaster <feildmaster@users.noreply.github.com>
2020-10-22 21:46:45 -0500

    chore: don't load hidden data on mobile

--- docs/assets/js/leaderboard.js
+++ docs/assets/js/leaderboard.js
@@ -1,45 +1,34 @@
-const columns = [{
-  key: 'rank',
-  format: (text = '', {rankChange: rc}) => `<span data-change="${rc === undefined ? '-' : rc}">${text}</span>`,
-}, {
-  key: 'level',
-}, {
-  key: 'username',
-  format: (text = '', { id }) => `<span title="ID: ${id}">${text}</span>`,
-}, {
-  key: 'division',
-  format: (text = '') => text.replace('_', ' '),
-}, {
-  key: 'eloRanked',
-  format: (text = '', { eloChange: ec }) => `<span data-change="${ec === undefined ? '-' : ec}">${text}</span>`,
-}, {
-  key: 'winsRanked',
-}, {
-  key: 'lossesRanked',
-}];
+import columns from './formatter.js';
 
-export function load(url) {
+// TODO: convert to elements
+function load(url) {
   // TODO: load partial entries over time
   return window.axios.get(url).then(ret => {
     const entries = ret.data.slice(5).map((entry) => columns.map(({
       key = '',
       format = (text, entry) => text,
-    }) => format(entry[key], entry))
-    .map((text) => `<td>${text}</td>`).join(''));
+    }) => `<td>${format(entry[key], entry)}</td>`).join(''));
 
     addEntries(entries);
   });
 }
 
-function addEntries(entries = [], limit = 100) {
-  if (!entries.length) return;
-  
-  const el = document.querySelector('.leaderboard tbody');
+// TODO: make a loader that detects when near the end of the list, and expand it
+function addEntries(entries = [], limit = 100, delay = 1000, safe, runs = 0) {
+  if (!entries.length || safe === false && runs >= 500) return;
+  if (safe === undefined) {
+    const el = document.querySelector('.mobile-warning');
+    safe = window.getComputedStyle(el).display === 'none';
+    if (safe) {
+      delay = 100;
+      limit = 500;
+    }
+  }
 
-  const text = entries.splice(0, limit).join('</tr></tr>');
-  el.innerHTML += `<tr>${text}</tr>`;
+  const text = `<tr>${entries.splice(0, limit).join('</tr></tr>')}</tr>`;
+  document.querySelector('.leaderboard tbody').innerHTML += text;
 
-  window.requestAnimationFrame(() => addEntries(entries, limit));
+  setTimeout(addEntries, delay, entries, limit, delay, safe, runs + limit);
 }
 
 load(dataURL);
